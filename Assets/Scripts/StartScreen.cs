0533c1e23b3f76e5edd896acbcc14fdddab266cc
Ricardo Bustamante <ricardobqueiroz@gmail.com>
2017-04-22 20:41:51 -0300

    Adding the music to the game.

--- Assets/Scripts/StartScreen.cs
+++ Assets/Scripts/StartScreen.cs
@@ -5,17 +5,15 @@ using UnityEngine.SceneManagement;
 
 public class StartScreen : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {            
-        if(Input.GetKeyDown(KeyCode.Return) || Input.GetAxis("Fire1") > 0)
-        {
-            SceneManager.LoadScene("proto_busta");
-        }
+  // Use this for initialization
+  void Start() {
 
+  }
+
+  // Update is called once per frame
+  void Update() {
+    if (Input.GetButtonDown("Fire1") || Input.GetButtonDown("P1_Fire") || Input.GetButtonDown("P2_Fire") || Input.GetKeyDown(KeyCode.KeypadEnter)) {
+      SceneManager.LoadScene("game_scene");
     }
+  }
 }
