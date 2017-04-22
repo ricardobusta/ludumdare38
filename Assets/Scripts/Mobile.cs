f17013e6c8b7570875e1238ece572e2b9bac355f
Ricardo Bustamante <ricardobqueiroz@gmail.com>
2017-04-21 23:39:30 -0300

    Mobile commit

---
+++ Assets/Scripts/Mobile.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mobile : MonoBehaviour {
+
+  public float r = 0;
+  public float a = 0;
+
+  // Use this for initialization
+  void Start() {
+
+  }
+
+  // Update is called once per frame
+  void Update() {
+    Vector3 p = new Vector3(r * Mathf.Cos(a * Mathf.Deg2Rad), r * Mathf.Sin(a * Mathf.Deg2Rad), 0);
+    transform.position = p;
+    transform.localRotation = Quaternion.Euler(0, 0, a);
+  }
+}
