e4996f5c6e2abb17b7ad4cc9931b25e6b53162c7
Daniel Augusto <danisson10@gmail.com>
2017-04-22 14:51:26 +0100

    Comentários

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerKeybind {
+  // Representa os keybinds de um player
   public string axis;
   public KeyCode shoot;
   public KeyCode jump;
@@ -16,19 +17,27 @@ public class PlayerKeybind {
 
 public class Player : MonoBehaviour {
 
+  // FIXME - Arrumar um lugar melhor pra guardar esses dados
   static PlayerKeybind[] keys = {
     new PlayerKeybind("P1_Horizontal",KeyCode.U,KeyCode.I),
     new PlayerKeybind("P2_Horizontal",KeyCode.Keypad0,KeyCode.KeypadPeriod)};
 
+  // Qual o número desse player
   public int playerN;
 
   Mobile mobile;
+
+  // Player está no chão?
   bool onGround = false;
+
+  // FIXME - Arrumar um lugar melhor pra guardar esse raio
   const float rPlaneta = 2;
 
   public float speed = 1;
   public float maxVSpeed = 3;
   public float gravity = 0.1f;
+
+  // Essa é a velocidade atual vertical
   private float vSpeed = 0;
 
   public float fireCD = 1;
@@ -45,36 +54,43 @@ public class Player : MonoBehaviour {
   // Update is called once per frame
   void Update () {
     float h = Input.GetAxis(keys[playerN].axis);
-    mobile.Move(h * speed);
 
     if (currentFireCD > 0) {
+      // Se o cooldown de tiro é positivo, decremente
       currentFireCD -= Time.deltaTime;
-    } else {
-      //float fire = Input.GetAxis("Fire1");
-      float fire = Input.GetKeyDown(keys[playerN].shoot)?1:0;
-      if (fire > 0) {
-        Bullet b = GameManager.Instance().GetFreeBullet();
-        if (b != null) {
-          b.gameObject.SetActive(true);
-          b.Activate(mobile);
-          currentFireCD = fireCD;
-        }
+    } else if (Input.GetKeyDown(keys[playerN].shoot)) {
+      // Senão, deixe o jogador atirar
+      Bullet b = GameManager.Instance().GetFreeBullet();
+      if (b != null) {
+        b.gameObject.SetActive(true);
+        b.Activate(mobile);
+        currentFireCD = fireCD;
       }
     }
 
-    onGround = (Mathf.Abs(mobile.radius) <= rPlaneta + 0.6);
+    // Está no chão se o raio for menor igual que o planeta + altura do jogador
+    onGround = (Mathf.Abs(mobile.radius) <= rPlaneta + 0.6f);
 
+    // Código para pulo
     if (onGround && Input.GetKeyDown(keys[playerN].jump)) {
       vSpeed = maxVSpeed;
       onGround = false;
     }
 
-    if (onGround)
+    if (onGround) {
+      // // Se está no chão, deixe o personagem no chão
       vSpeed = 0;
-    else
+      mobile.radius = rPlaneta + 0.6f;
+    } else {
+      // Senão, aplique gravidade
       vSpeed -= gravity;
-    
+    }
+
+    // Cálculo de posição vertical e horizontal
     mobile.radius += vSpeed * Time.deltaTime;
+    mobile.Move(h * speed);
+
+
     Debug.LogFormat("Player{5}: p({0},{1}) v({4},{3}) {2}",mobile.angle, mobile.radius, onGround,vSpeed,h * speed, playerN);
 	}
 }
