2f2fc1542493326d0a236e42cd3474344ac83c74
Joaquim Alvino de Mesquita Neto <joaquimmnetto@gmail.com>
2017-04-22 12:16:06 -0300

    Tela inicial feita

---
+++ Assets/Scripts/BlinkText.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BlinkText : MonoBehaviour {
+
+    public float upperLimit = 1;
+    public float lowerLimit = 0.1f;
+    public float fadeRatio = 0.95f;
+
+    Text txt = null;
+       // Use this for initialization
+    void Start () {
+        txt = GetComponent<Text>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        
+        if(txt.color.r < lowerLimit || txt.color.g < lowerLimit || txt.color.b < lowerLimit)
+        {
+            fadeRatio = 1 + fadeRatio;
+        }
+        else if (txt.color.r > upperLimit || txt.color.g > upperLimit || txt.color.b > upperLimit)
+        {
+            fadeRatio = 1 - fadeRatio;
+        }
+
+        txt.color = new Color(txt.color.r * fadeRatio, txt.color.g * fadeRatio, txt.color.b * fadeRatio);
+    }
+}
