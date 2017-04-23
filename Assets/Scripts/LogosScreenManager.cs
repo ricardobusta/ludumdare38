f4229e8e910ee904ffd32a0be67948bb0b75b5bb
Ricardo Bustamante <ricardobqueiroz@gmail.com>
2017-04-23 11:34:02 -0300

    Now the players can select the main menu button by pressing fire button.

---
+++ Assets/Scripts/LogosScreenManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LogosScreenManager : MonoBehaviour {
+
+  float timer = 2;
+
+	void Update () {
+    timer -= Time.deltaTime;
+
+    if (timer <= 0 || Input.GetButton("P1_Fire") || Input.GetButton("P2_Fire") || Input.GetButton("Submit")) {
+      SceneManager.LoadScene("title_screen");
+    }
+
+  }
+}
--- Assets/Scripts/StartScreen.cs
+++ Assets/Scripts/StartScreen.cs
@@ -1,10 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class StartScreen : MonoBehaviour {
+
+  EventSystem eventSystem;
+
+  private void Start() {
+    eventSystem = FindObjectOfType<EventSystem>();
+  }
+
   public void StartGame() {
     SceneManager.LoadScene("game_scene");
   }
@@ -19,5 +27,15 @@ public class StartScreen : MonoBehaviour {
 
   public void Exit() {
     Application.Quit();
+  }
+
+  private void Update() {
+    if (Input.GetButton("P1_Fire") || Input.GetButton("P2_Fire")) {
+      GameObject focus = eventSystem.currentSelectedGameObject;
+      if (focus != null) {
+        var pointer = new PointerEventData(EventSystem.current);
+        ExecuteEvents.Execute(focus, pointer, ExecuteEvents.submitHandler);
+      }
+    }
   }
 }
