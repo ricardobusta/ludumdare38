45c2039bd4c75d833eb607b3729466e4f614652f
Ricardo Bustamante <ricardobqueiroz@gmail.com>
2017-04-23 10:33:50 -0300

    CHange hitbox?

--- Assets/Scripts/StartScreen.cs
+++ Assets/Scripts/StartScreen.cs
@@ -2,18 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class StartScreen : MonoBehaviour {
 
-  // Use this for initialization
-  void Start() {
-
-  }
-
-  // Update is called once per frame
-  void Update() {
-    if (Input.GetButtonDown("Fire1") || Input.GetButtonDown("P1_Fire") || Input.GetButtonDown("P2_Fire") || Input.GetKeyDown(KeyCode.KeypadEnter)) {
-      SceneManager.LoadScene("game_scene");
-    }
-  }
 }
