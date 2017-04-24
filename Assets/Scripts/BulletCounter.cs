33ac45cea30716119f3ee5ee81a43786d16b9ee7
Ricardo Bustamante <ricardobqueiroz@gmail.com>
2017-04-24 20:13:23 -0300

    Adicionando uma porrada de asset, implementando os contadores de bala.

--- Assets/Scripts/BulletCounter.cs
+++ Assets/Scripts/BulletCounter.cs
@@ -7,21 +7,29 @@ public class BulletCounter : MonoBehaviour {
 
   public Text text;
   public GameObject[] bullets;
+  public Image image;
+
+  const float spacing = 50;
 
   public int bulletCount;
 
+  //private void Update() {
+  //  SetBulletCount(bulletCount);
+  //}
+
   public void SetBulletCount(int count) {
-    if(count > 6) {
-      text.gameObject.SetActive(true);
-      text.text = count.ToString();
+    if(count > bullets.Length) {
+      image.gameObject.SetActive(true);
+      text.text = "x"+count.ToString();
       bullets[0].SetActive(true);
       for(int i = 1; i < bullets.Length; i++) {
         bullets[i].SetActive(false);
       }
     } else {
-      text.gameObject.SetActive(false);
+      image.gameObject.SetActive(false);
       for (int i = 0; i < bullets.Length; i++) {
         bullets[i].SetActive(i<count);
+        bullets[i].transform.localPosition = new Vector3(i*spacing, 0, 0);
       }
     }
   }
