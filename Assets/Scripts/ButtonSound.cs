74959329eaf100b153127b604b3aed0b2023f000
Ricardo Bustamante <ricardobqueiroz@gmail.com>
2017-04-24 13:46:18 -0300

    Adicionando somzinho da interface.

---
+++ Assets/Scripts/ButtonSound.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ButtonSound : MonoBehaviour, IPointerEnterHandler, IPointerDownHandler {
+  public void OnPointerEnter(PointerEventData ped) {
+    AudioManager.Instance().PlayFire();
+  }
+
+  public void OnPointerDown(PointerEventData ped) {
+    AudioManager.Instance().PlayFire();
+  }
+}
