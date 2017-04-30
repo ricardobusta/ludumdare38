d884e244d95853902ac6efee5e4511bea8564a66
Ricardo Bustamante <ricardobqueiroz@gmail.com>
2017-04-30 14:21:09 -0300

    Adding player animation generic system. Now you can just set the script parameters and the player will have a different sprite.

---
+++ Assets/Scripts/Game/SpriteSwapper.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+/// <summary>
+/// Makes it easier to swap sprites at runtime.
+/// </summary>
+public class SpriteSwapper : MonoBehaviour {
+
+  public float currentSprite;
+  public Sprite[] spriteSheet;
+
+  /// <summary>
+  /// SpriteRenderer cache
+  /// </summary>
+  private SpriteRenderer spriteRenderer;
+
+  /// <summary>
+  /// 
+  /// </summary>
+  void Start() {
+    this.spriteRenderer = GetComponent<SpriteRenderer>();
+  }
+
+  /// <summary>
+  /// 
+  /// </summary>
+  private void LateUpdate() {
+    // Swap out the sprite to be rendered by its name
+    print(currentSprite);
+    this.spriteRenderer.sprite = spriteSheet[(int)Mathf.Ceil(currentSprite)];
+  }
+}
